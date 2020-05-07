b8841e582247a462b7bca147612eecd1a7847bad
Christopher Biscardi <chris@christopherbiscardi.com>
2020-05-07 15:28:30 -0700

    fork snowpack

--- .github/ISSUE_TEMPLATE.md
+++
@@ -1,5 +0,0 @@
-To create a new issue or search existing discussions, start here:  
-👉 **https://www.pika.dev/packages/snowpack/discuss** 👈
-
-All new issues created directly through GitHub will be closed.  
-Learn more: https://www.pika.dev/discussions
--- assets/babel-plugin.js
+++
@@ -1,7 +0,0 @@
-console.error('snowpack/assets/babel-plugin.js has moved!');
-console.error('');
-console.error('  npm install --save-dev @snowpack/babel-plugin-package-import');
-console.error('');
-console.error(
-  'Learn more: https://github.com/pikapkg/create-snowpack-app/tree/master/packages/babel-plugin-package-import',
-);
